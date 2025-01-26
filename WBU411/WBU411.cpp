dc81ac397b419a08b78c8b668a7152869ed9aa9a
Nikolz <126600682+Kebbot@users.noreply.github.com>
2025-01-26 11:23:13 +0300

    Commit#6
    Para 1 - 26.01.2025

--- WBU411/WBU411.cpp
+++ WBU411/WBU411.cpp
@@ -4,6 +4,38 @@
 #include <cmath>
 using namespace std;
 
+// Перечисляемый тип
+enum Seasons {
+    WINTER,SPRING, SUMMER,AUTUMN
+};
+enum Months {
+    JAN=1,FEB,MAR,APR,MAY,
+    JUN,JUL,AUG,SEP,OCT,NOV,DEC
+};
+enum Signal {
+    OFF,ON
+};
+
+// Указатели на функции - тема
+int myFunc()
+{
+    return 10;
+}
+char* pool(char* str, int size)
+{
+    return str;
+}
+
+// Ключевое слово auto 
+auto minFunc()
+{
+    auto a = 20;
+    auto b = 10;
+    if (a < b)
+        return a + 10.5;
+    else
+        return b + 10.5;
+}
 
 int main() 
 {
@@ -12,27 +44,32 @@ int main()
     setlocale(LC_ALL, "Rus");
     srand(time(NULL));
     
-    int row = 5;
-    int** mass = new int* [row];
-    int col[5];
-    
-    for (int i = 0; i < row; i++)
-    {
-        cout << "Введите размерность " << i + 1 << " строки: ";
-        cin >> col[i];
-        mass[i] = new int[col[i]];
-    }
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col[i]; j++)
-        {
-            mass[i][j] = rand() % 100;
-            cout << mass[i][j] << ' ';
-        }
-        cout << endl;
-    }
+    //cout << myFunc << endl; //адрес фунции 
+    //cout << &myFunc << endl;  //другой способ - адрес фунции
+
+    ////Указатели на функции
+    //int (*funcPtr)(); //создание указателя
+    //funcPtr = &myFunc; //инициализация
+
+    //char* (*funcPtr2)(char*, int); //создание указателя
+    //funcPtr2 = pool;  //инициализация
+
+    // Ключевое слово auto 
+    auto num = 10; // целочисленный тип
+    auto num2 = 4.5; // вещественный тип
+    auto sum = 4.5 + 10; // выражение
+    auto ptr = &sum; // указатель
+    auto minf = minFunc();
+    cout << minf;
+
+    // Ключевое слово decltype 
+    int x = 1.2;
+    decltype(x) j;
+   
+    int y = 10;
+    int& r = y;
+    //decltype(r) xy;
+
    
     return 0;
 }
-//указатель_на_тип = new имя_типа (инициализатор)
-//след тема = перечисляемые типы (урок 15)
